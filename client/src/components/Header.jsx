0651fa4002ee1803c6d420133f3327f55403dcdf
Aryan Tiwari <77456196+aryantiw@users.noreply.github.com>
2024-05-03 08:07:52 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,8 +35,8 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">MERN</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">Habitat</span>
+            <span className="text-slate-700">Haven</span>
           </h1>
         </Link>
         <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
